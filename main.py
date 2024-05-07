1937ea446541eb688c5d43123b59d54cf879943f
pplulee <hi@pplulee.me>
2024-05-07 23:23:09 +0100

    feat: ban user

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from traceback import print_exc
 
 import telebot
 from telebot import types
-from telebot.apihelper import create_forum_topic, close_forum_topic, ApiTelegramException, delete_forum_topic
+from telebot.apihelper import create_forum_topic, close_forum_topic, ApiTelegramException, delete_forum_topic, \
+    reopen_forum_topic
 from telebot.types import Message
 
 from cache import CacheHelper
@@ -56,6 +57,8 @@ class TGBot:
         self.group_id = int(group_id)
         self.bot = telebot.TeleBot(bot_token)
         self.bot.message_handler(commands=["help"])(self.help)
+        self.bot.message_handler(commands=["ban"])(self.ban_user)
+        self.bot.message_handler(commands=["unban"])(self.unban_user)
         self.bot.message_handler(commands=["terminate"])(self.handle_terminate)
         self.bot.message_handler(func=lambda m: True, content_types=["photo", "text", "sticker", "video", "document"])(
             self.push_messages)
@@ -142,6 +145,8 @@ class TGBot:
                 self.bot.reply_to(message, _("Failed to terminate the thread"))
 
     def match_auto_response(self, text):
+        if text is None:
+            return None
         with sqlite3.connect(self.db_path) as db:
             # Check for exact match
             db_cursor = db.cursor()
@@ -174,6 +179,12 @@ class TGBot:
                 logger.info(
                     _("Received message from {}, content: {}, type: {}").format(message.from_user.id, message.text,
                                                                                 message.content_type))
+                # Check if the user is banned
+                result = curser.execute("SELECT ban FROM topics WHERE user_id = ? LIMIT 1", (message.from_user.id,))
+                if (result := result.fetchone()) is not None:
+                    if result[0] == 1:
+                        logger.info(_("User {} is banned").format(message.from_user.id))
+                        return
                 # Auto response
                 if (auto_response_result := self.match_auto_response(message.text)) is not None:
                     self.bot.send_message(message.chat.id, auto_response_result["response"])
@@ -211,15 +222,14 @@ class TGBot:
                                                         f"Username: {username}\n",
                                                         message_thread_id=thread_id)
                     self.bot.pin_chat_message(self.group_id, pin_message.message_id)
+                else:
+                    thread_id = thread_id[0]
                 try:
                     self.bot.forward_message(self.group_id, message.chat.id, message_thread_id=thread_id,
                                              message_id=message.message_id)
-                except ApiTelegramException as e:
-                    logger.error(_("Failed to forward message from user {}".format(message.from_user.id)) + f" {e}")
-                    self.bot.send_message(self.group_id,
-                                          _("Failed to forward message from user {}".format(message.from_user.id)),
-                                          message_thread_id=None)
-                    self.bot.forward_message(self.group_id, message.chat.id, message_id=message.message_id)
+                except ApiTelegramException:
+                    self.terminate_thread(thread_id=thread_id)
+                    return self.handle_message(message)
                 if topic_action:
                     self.bot.send_message(self.group_id, _("[Auto Response]") + auto_response,
                                           message_thread_id=thread_id)
@@ -308,8 +318,8 @@ class TGBot:
         if message.text == "/cancel":
             self.bot.send_message(self.group_id, _("Operation cancelled"))
             return
-        msg = self.bot.send_message(text=_("Please send the response message."),
-                                    chat_id=self.group_id, message_thread_id=None)
+        msg = self.bot.edit_message_text(text=_("Please send the response message."),
+                                         chat_id=self.group_id, message_id=message.message_id)
         self.bot.register_next_step_handler(msg, self.add_auto_response_topic_action)
 
     def add_auto_response_topic_action(self, message: Message):
@@ -368,8 +378,8 @@ class TGBot:
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton("💬" + _("Auto Reply"),
                                               callback_data=json.dumps({"action": "auto_reply"})))
-        # markup.add(types.InlineKeyboardButton("⛔" + _("Banned Users"),
-        #                                       callback_data=json.dumps({"action": "ban_user"})))
+        markup.add(types.InlineKeyboardButton("⛔" + _("Banned Users"),
+                                              callback_data=json.dumps({"action": "ban_user"})))
         if edit:
             self.bot.edit_message_text(_("Menu"), message.chat.id, message.message_id, reply_markup=markup)
         else:
@@ -435,6 +445,94 @@ class TGBot:
         self.bot.edit_message_text(_("Auto reply deleted"), chat_id=message.chat.id, message_id=message.id,
                                    reply_markup=markup)
 
+    def ban_user(self, message: Message):
+        if self.check_valid_chat(message):
+            self.bot.send_message(self.group_id, _("This command is not available in the main chat."))
+            return
+        with sqlite3.connect(self.db_path) as db:
+            db_cursor = db.cursor()
+            db_cursor.execute("UPDATE topics SET ban = 1 WHERE thread_id = ?", (message.message_thread_id,))
+            db.commit()
+        self.bot.send_message(self.group_id, _("User banned"), message_thread_id=message.message_thread_id)
+        close_forum_topic(chat_id=self.group_id, message_thread_id=message.message_thread_id, token=self.bot.token)
+
+    def unban_user(self, message: Message, user_id: int = None):
+        if user_id is None:
+            if self.check_valid_chat(message):
+                if len((msg_split := message.text.split(" "))) != 2:
+                    self.bot.reply_to(message, "Invalid command\n"
+                                               "Correct usage:```\n"
+                                               "/unban <user ID>```", parse_mode="Markdown")
+                    return
+                user_id = int(msg_split[1])
+        if user_id is None:
+            with sqlite3.connect(self.db_path) as db:
+                db_cursor = db.cursor()
+                db_cursor.execute("UPDATE topics SET ban = 0 WHERE thread_id = ?", (message.message_thread_id,))
+                db.commit()
+            self.bot.send_message(self.group_id, _("User unbanned"), message_thread_id=message.message_thread_id)
+            try:
+                reopen_forum_topic(chat_id=self.group_id, message_thread_id=message.message_thread_id,
+                                   token=self.bot.token)
+            except ApiTelegramException:
+                pass
+        else:
+            with sqlite3.connect(self.db_path) as db:
+                db_cursor = db.cursor()
+                # Check user exists
+                db_cursor.execute("SELECT thread_id FROM topics WHERE user_id = ? LIMIT 1", (user_id,))
+                thread_id = db_cursor.fetchone()
+                if thread_id is None:
+                    self.bot.send_message(self.group_id, _("User not found"))
+                    return
+                db_cursor.execute("UPDATE topics SET ban = 0 WHERE user_id = ?", (user_id,))
+                db.commit()
+            try:
+                reopen_forum_topic(chat_id=self.group_id, message_thread_id=thread_id,
+                                   token=self.bot.token)
+            except ApiTelegramException:
+                pass
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton("⬅️" + _("Back"), callback_data=json.dumps({"action": "menu"})))
+            if message.from_user.id == self.bot.get_me().id:
+                self.bot.edit_message_text(_("User unbanned"), message.chat.id, message.message_id, reply_markup=markup)
+            else:
+                self.bot.send_message(self.group_id, _("User unbanned"), reply_markup=markup)
+
+    def manage_ban_user(self, message: Message):
+        with sqlite3.connect(self.db_path) as db:
+            db_cursor = db.cursor()
+            markup = types.InlineKeyboardMarkup()
+            back_button = types.InlineKeyboardButton("⬅️" + _("Back"), callback_data=json.dumps({"action": "menu"}))
+            db_cursor.execute("SELECT user_id FROM topics WHERE ban = 1")
+            banned_users = db_cursor.fetchall()
+            text = _("Banned User List:\n")
+            for user in banned_users:
+                text += "-" * 20 + "\n"
+                text += f"User ID: {user[0]}\n"
+                markup.add(types.InlineKeyboardButton(text=user[0],
+                                                      callback_data=json.dumps(
+                                                          {"action": "select_ban_user", "id": user[0]})))
+            markup.add(back_button)
+            self.bot.edit_message_text(text, message.chat.id, message.message_id, reply_markup=markup)
+
+    def select_ban_user(self, message: Message, id: int):
+        with sqlite3.connect(self.db_path) as db:
+            db_cursor = db.cursor()
+            db_cursor.execute("SELECT thread_id FROM topics WHERE user_id = ? LIMIT 1", (id,))
+            thread_id = db_cursor.fetchone()
+            if thread_id is None:
+                self.bot.send_message(self.group_id, _("User not found"))
+                return
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton("❌" + _("Unban"),
+                                                  callback_data=json.dumps({"action": "unban_user", "id": id}))
+                       )
+            markup.add(types.InlineKeyboardButton("⬅️" + _("Back"),
+                                                  callback_data=json.dumps({"action": "manage_ban_user"})))
+            text = _("User ID: {}\n").format(id)
+            self.bot.edit_message_text(text, message.chat.id, message.message_id, reply_markup=markup)
+
     def callback_query(self, call: types.CallbackQuery):
         if call.data == "null":
             logger.error(_("Invalid callback data received"))
@@ -487,8 +585,20 @@ class TGBot:
                     self.bot.send_message(self.group_id, _("Invalid action"), reply_markup=markup)
                     return
                 self.delete_auto_reply(call.message, data["id"])
-            # case "ban_user":
-            #     pass
+            case "ban_user":
+                self.manage_ban_user(call.message)
+            case "unban_user":
+                if "id" not in data:
+                    self.bot.delete_message(self.group_id, call.message.message_id)
+                    self.bot.send_message(self.group_id, _("Invalid action"), reply_markup=markup)
+                    return
+                self.unban_user(call.message, user_id=data["id"])
+            case "select_ban_user":
+                if "id" not in data:
+                    self.bot.delete_message(self.group_id, call.message.message_id)
+                    self.bot.send_message(self.group_id, _("Invalid action"), reply_markup=markup)
+                    return
+                self.select_ban_user(call.message, data["id"])
             case _:
                 logger.error(_("Invalid action received"))
         return
